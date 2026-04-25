2b7fb0189ef808a7aed885155b8dd4d487b391a9
paulszyburski <paul.szyburski@gmail.com>
2026-04-25 13:28:53 +0200

    clean utils logs-use later for input

--- simulate.py
+++ simulate.py
@@ -1,16 +1,17 @@
 import pybullet as p
-from utils import find_joints, track_held_keys
+from utils import find_joints, track_held_keys, get_pos, get_orientation, compute_power, compute_corners_position, get_velocity, get_steer_angle
 import pybullet_data
 import time
 
-p.connect(p.GUI)
+client_id = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.81)
 
 plane = p.loadURDF("plane.urdf")
-car = p.loadURDF("racecar/racecar.urdf", [0, 0, 0.2])
+car = p.loadURDF("racecar/racecar.urdf", [0, 0, 0])
 car2 = p.loadURDF("racecar/racecar.urdf", [0.5, 0, 0.2])
 car3 = p.loadURDF("racecar/racecar.urdf", [-0.5, 0, 0.2])
+
 
 steering_joints, drive_joints = find_joints(car)
 keys_held = set()
@@ -56,6 +57,27 @@ def main():
 
         track_held_keys(events, keys_held)
         control()
+
+        position = get_pos(car, client_id)
+        orientation = get_orientation(car, client_id)[2]
+        front_middle, front_right, front_left, back_middle, back_right, back_left = compute_corners_position(position, orientation)
+        velocity = get_velocity(car, client_id)
+        steer_angle = get_steer_angle(car, steering_joints)
+        
+
+        if step % 550 == 0:
+            print(f"X: {position[0]:.2f}, Y: {position[1]:.2f}, Z: {position[2]:.2f}")
+            print(f"Orientation yaw: {compute_power(str(orientation))}")
+            print(f"Steer angle: {compute_power(str(steer_angle))}")
+            print(f"Velocity: {compute_power(str(velocity[0]))}, {compute_power(str(velocity[1]))}, {compute_power(str(velocity[2]))}")
+            print(f"Front middle: {compute_power(str(front_middle[0]))}, {compute_power(str(front_middle[1]))}")
+            print(f"Front right: {compute_power(str(front_right[0]))}, {compute_power(str(front_right[1]))}")
+            print(f"Front left: {compute_power(str(front_left[0]))}, {compute_power(str(front_left[1]))}")
+            print(f"Back middle: {compute_power(str(back_middle[0]))}, {compute_power(str(back_middle[1]))}")
+            print(f"Back right: {compute_power(str(back_right[0]))}, {compute_power(str(back_right[1]))}")
+            print(f"Back left: {compute_power(str(back_left[0]))}, {compute_power(str(back_left[1]))}\n\n")
+
+        step += 1
 
         p.stepSimulation()
         time.sleep(1 / 240)
